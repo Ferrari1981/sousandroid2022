7a03085c9167587b6cf461e366682d2e0e61d9a5
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2022-03-17 10:54:18 +0300

    one commit --17,03,2022---10.54--раб удалена ошибка при перключение между фрагментами destoyview

--- app/src/main/java/com/dsy/dsu/Code_For_Tasks_КодДля_Задания/Fragment3_Now_Create_Tasks.java
+++ app/src/main/java/com/dsy/dsu/Code_For_Tasks_КодДля_Задания/Fragment3_Now_Create_Tasks.java
@@ -3,6 +3,7 @@ package com.dsy.dsu.Code_For_Tasks_КодДля_Задания;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DataSetObserver;
 import android.database.sqlite.SQLiteCursor;
 import android.graphics.Color;
@@ -193,6 +194,11 @@ public class Fragment3_Now_Create_Tasks extends Fragment1_One_Tasks {
 
             // TODO: 05.03.2022  ДЛЯ ИНИЗАЛИЗАЦИИ НИЖНИХ КНОПОК
             subClassBuccessLogin_главныйКлассБизнесЛогикиФрагмент3ЗаполенияЗадачиДляСозданияНовойЗадачи.МетодСозданиеНавигаторКнопок();
+
+            // TODO: 17.03.2022
+
+            // TODO: 05.03.2022  СТАТУС ЗНАЧКА С ДОПОЛНИТЕЛЬНЫЙ СТАТУСОМ
+            subClassBuccessLogin_главныйКлассБизнесЛогикиФрагмент3ЗаполенияЗадачиДляСозданияНовойЗадачи.МетодКпопкаСоЗачкомКраснымДополнительныйСтатус(Курсор_ДляПолученияДАнныхТОлькоДляЗадачВработе);
 
 
             Log.d(this.getClass().getName(), " нет данных для отображения " +
@@ -732,7 +738,52 @@ public class Fragment3_Now_Create_Tasks extends Fragment1_One_Tasks {
         }
 
         // TODO: 14.03.2022
+        // TODO: 14.03.2022
 
+        private void МетодКпопкаСоЗачкомКраснымДополнительныйСтатус(@NonNull Cursor cursorДЛяОпределенияНужноПоказыватьЗначеиЛИнЕТ)
+                throws ExecutionException, InterruptedException {
+            // TODO: 02.03.2022
+            try {
+                // TODO: 02.03.2022
+                Log.d(this.getClass().getName(), "  cursorДЛяОпределенияНужноПоказыватьЗначеиЛИнЕТ " + cursorДЛяОпределенияНужноПоказыватьЗначеиЛИнЕТ);
+                // TODO: 09.03.2022
+                if (cursorДЛяОпределенияНужноПоказыватьЗначеиЛИнЕТ.getCount() > 0) {
+                    // TODO: 06.03.2022
+                    bottomNavigationViewДляTasks.getOrCreateBadge(R.id.id_taskCreateNewTasks).setVisible(true);//.getOrCreateBadge(R.id.id_taskHome).setVisible(true); R.id.id_taskHome todo R.id.id_taskCreateNewTasks
+                    // TODO: 06.03.2022
+                    bottomNavigationViewДляTasks.getOrCreateBadge(R.id.id_taskCreateNewTasks).setNumber(Курсор_ДляПолученияДАнныхТОлькоДляЗадачВработе.getCount());//.getOrCreateBadge(R.id.id_taskHome).setVisible(true);
+                    // TODO: 09.03.2022
+                    bottomNavigationViewДляTasks.getOrCreateBadge(R.id.id_taskCreateNewTasks).setBackgroundColor(Color.parseColor("#8B0000"));
+                    // TODO: 06.03.2022
+                    Log.d(this.getClass().getName(), "  bottomNavigationViewДляTasks " + bottomNavigationViewДляTasks +
+                            "  Курсор_ДляПолученияДАнныхТОлькоДляЗадачВработе.getCount()   " + Курсор_ДляПолученияДАнныхТОлькоДляЗадачВработе.getCount());
+                    // TODO: 05.03.2022
+                } else {
+                    // TODO: 06.03.2022
+                    bottomNavigationViewДляTasks.getOrCreateBadge(R.id.id_taskCreateNewTasks).setVisible(false);//.getOrCreateBadge(R.id.id_taskHome).setVisible(true);
+                    // TODO: 09.03.2022
+                    bottomNavigationViewДляTasks.getOrCreateBadge(R.id.id_taskCreateNewTasks).setNumber(Курсор_ДляПолученияДАнныхТОлькоДляЗадачВработе.getCount());
+                    // TODO: 10.03.2022
+                    bottomNavigationViewДляTasks.getOrCreateBadge(R.id.id_taskCreateNewTasks).setBackgroundColor(Color.BLACK);
+                }
+                Log.d(this.getClass().getName(), "  bottomNavigationViewДляTasks " + bottomNavigationViewДляTasks +
+                        "  Курсор_ДляПолученияДАнныхТОлькоДляЗадачВработе.getCount()   " + Курсор_ДляПолученияДАнныхТОлькоДляЗадачВработе);
+                // TODO: 13.03.2022
+                bottomNavigationViewДляTasks.requestLayout();
+                // TODO: 13.03.2022
+                bottomNavigationViewДляTasks.requestApplyInsets();
+                // TODO: 14.03.2022
+                linearLayou.requestLayout();
+                /////////////
+            } catch (Exception e) {
+                e.printStackTrace();
+                ///метод запись ошибок в таблицу
+                Log.e(this.getClass().getName(), "Ошибка " + e + " Метод :" + Thread.currentThread().getStackTrace()[2].getMethodName() +
+                        " Линия  :" + Thread.currentThread().getStackTrace()[2].getLineNumber());
+                new Class_Generation_Errors(getContext()).МетодЗаписиВЖурналНовойОшибки(e.toString(), this.getClass().getName(),
+                        Thread.currentThread().getStackTrace()[2].getMethodName(), Thread.currentThread().getStackTrace()[2].getLineNumber());
+            }
+        }
 
         // TODO: 15.03.2022  перенесееный код
         // TODO: 28.02.2022 начало  MyViewHolder
